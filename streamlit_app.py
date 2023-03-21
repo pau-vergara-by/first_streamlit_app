2022c97d666edce28e529868431d09dba6d79e54
pau-vergara-by <102988291+pau-vergara-by@users.noreply.github.com>
2023-03-21 16:45:36 -0600

    Update streamlit_app.py
    kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,6 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
-streamlit.text(fruityvice_response.json()) #insert the json data from watermelon
 
 # normalize json response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
