db3b3103f2dcf1be472a82a02156d5582ecbb976
pau-vergara-by <102988291+pau-vergara-by@users.noreply.github.com>
2023-03-21 16:39:57 -0600

    Update streamlit_app.py
    normalize

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,11 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) #insert the json data from watermelon
+
+# normalize json response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# insert in the screen
+streamlit.dataframe(fruityvice_normalized)
+
+
