25f0bf380e5a30d5895c53d02cad6c44fe61c795
pau-vergara-by <102988291+pau-vergara-by@users.noreply.github.com>
2023-03-22 17:59:49 -0600

    Update streamlit_app.py
    design

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,7 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
+
+my_data_row = my_xur.fetchone()
+streamlit.header("The fruit load list contains:")
+streamlist.dataframe(my_data_row)
